7624384e5e50b219d9fd58aa71175d53a3f88dd4
NoorZak <n.zakarneh1@student.aaup.edu>
2021-01-10 21:26:18 +0200

    nindoj

---
+++ DojosAndNinjas/src/test/java/com/spring2/dojosninjas/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring2.dojosninjas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
